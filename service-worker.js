6da7508dddd21394bb16056a22f884531892ff3e
Gautams-GitHub <gagan.off.the.grid@gmail.com>
2024-08-01 23:10:16 +0530

    Remove incorrect caching

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,26 @@
 // service-worker.js
 
-const CACHE_NAME = 'pwa-card-app-cache-v1';
+const CACHE_NAME = "pwa-card-app-cache-v1";
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './styles.css',
-    './script.js',
-    './data.json',
-    
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+  "./script.js",
 ];
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
